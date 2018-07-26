3a7073da9b89a19d51813ac9e138d0d54fe2fcf8
adithyagaurav <41729963+adithyagaurav@users.noreply.github.com>
2018-07-27 03:01:38 +0530

    Add files via upload

---
+++ Reciever.ino
@@ -0,0 +1,103 @@
+#include <Servo.h> //include servo library
+Servo myservo; // create servo object to control a servo
+int q=11; //data pin Do from decoder
+int w=12; //data pin D1 from decoder
+int e=13; //data pin D2 from decoder
+int potpin = 10; //data pin from decoder
+int Vccpin= A4; // Vcc for decoder and receiver
+int Gndpin=A5; // Gnd for decoder and receiver
+int V=A0; //Vcc for L293D IC
+int G=A1; //Gnd for L293D IC
+int a=2; // motor1 terminal 1 connected to digital pin 2
+int b=3; // motor1 terminal 2 connected to digital pin 3
+int c=8; // motor2 terminal 1 connected to digital pin 8
+int d=9; // motor2 terminal 2 connected to digital pin 9
+int val; // variable to read the value from the analog pin
+void setup()
+{
+ Serial.begin(9600);
+ myservo.attach(6); // attaches the servo on pin 9 to the servo object
+ pinMode(potpin,INPUT);
+ pinMode(Gndpin,OUTPUT);
+ pinMode(Vccpin, OUTPUT);
+ pinMode(V, OUTPUT);
+ pinMode(G, OUTPUT);
+ pinMode(q, INPUT);
+ pinMode(w, INPUT);
+ pinMode(e, INPUT);
+ pinMode(a, OUTPUT);
+ pinMode(b, OUTPUT);
+ pinMode(c, OUTPUT);
+ pinMode(d, OUTPUT);
+ digitalWrite(Gndpin,LOW);
+ digitalWrite(Vccpin,HIGH);
+digitalWrite(V, HIGH);
+digitalWrite(G, LOW);
+}
+void loop()
+{
+ val=digitalRead(potpin);
+ int val1=digitalRead(q);
+ int val2=digitalRead(w);
+ int val3=digitalRead(e);
+ if (val1==1 && val2==1 && val3==1)
+ {
+ val =map (val, 0,1,0,100);
+ Serial.println(val);
+ myservo.write(val);
+ digitalWrite(a, HIGH);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, LOW);
+ Serial.println(" ");
+ Serial.println("forward");
+ }
+ if (val1==0 && val2==1 && val3==0)
+ {
+ val = map (val, 0,1,0,100);
+ Serial.println(val);
+ myservo.write(val);
+ Serial.println(" ");
+ Serial.println("backward");
+ digitalWrite(a, LOW);
+ digitalWrite(b, HIGH);
+ digitalWrite(c, LOW);
+ digitalWrite(d, LOW);
+ }
+ if (val1==1 && val2==0 && val3==0)
+ {
+ val =map (val, 0,1,0,100);
+ Serial.println(val);
+ myservo.write(val);
+ Serial.println("");
+ Serial.println("right");
+ digitalWrite(a, LOW);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, HIGH);
+ }
+ if (val1==0 && val2==0 && val3==0)
+ {
+ val =map (val, 0,1,0,100);
+ Serial.println(val);
+ myservo.write(val);
+ Serial.println("");
+ Serial.println("stop");
+ digitalWrite(a, LOW);
+ digitalWrite(b, LOW);
+ digitalWrite(c, LOW);
+ digitalWrite(d, LOW);
+ }
+ if (val1==0 && val2==0 && val3==1)
+ {
+ val =map (val, 0,1,0,100);
+ Serial.println(val);
+ myservo.write(val);
+ Serial.println(" ");
+ Serial.println("left");
+ digitalWrite(a, LOW);
+ digitalWrite(b, LOW);
+ digitalWrite(c, HIGH);
+ digitalWrite(d, LOW);
+ }
+}
---
+++ Transmitter.ino
@@ -0,0 +1,166 @@
+
+int GNDPin=A4; //Set Analog pin 4 as GND
+int VccPin=A5; //Set Analog pin 5 as VCC
+int GNDPina=A0;
+int xPin=A3; //X axis input
+int yPin=A2; //Y axis input
+int Q1=7;
+int Q2=8;
+int Q3=9;
+const int f=A1;
+int v;
+const int t=2;
+long x; //Variabe for storing X coordinates
+long y; //Variabe for storing Y coordinates
+long z; //Variabe for storing Z coordinates
+void setup()
+{
+  Serial.begin(9600);
+
+              
+  
+  pinMode(GNDPin, OUTPUT);
+  pinMode(VccPin, OUTPUT);
+  pinMode(GNDPina, OUTPUT);
+  pinMode(Q1,OUTPUT);
+  pinMode(Q2,OUTPUT);
+  pinMode(Q3,OUTPUT);
+  pinMode(f, INPUT);
+  pinMode(t, OUTPUT);
+  digitalWrite(GNDPin, LOW); //Set A4 pin LOW
+  digitalWrite(VccPin, HIGH); //Set A5 pin HIGH
+  digitalWrite(GNDPina, LOW); 
+}
+void loop()
+{
+  x = analogRead(xPin); //Reads X coordinates
+  y = analogRead(yPin); //Reads Y coordinates
+ 
+    
+     if ( y>370)// Change the value for adjusting sensitivity
+      forward();
+      else if  (y<294)
+      backward();
+    else if(x>400)  // Change the value for adjusting sensitivity
+      right();
+    else if(x<300)  // Change the value for adjusting sensitivity
+      left();
+      else
+      stop_();
+    
+}
+
+  
+
+void forward()
+{
+  v=analogRead(f);
+  if(v<550)
+  {
+  Serial.println("");
+  Serial.println("Forward Close");
+  digitalWrite(Q1,HIGH);
+  digitalWrite(Q2,HIGH);
+  digitalWrite(Q3,HIGH);
+  digitalWrite(t, HIGH);
+}
+if(v>551)
+  {
+  Serial.println("");
+  Serial.println("Forward Open");
+  digitalWrite(Q1,HIGH);
+  digitalWrite(Q2,HIGH);
+  digitalWrite(Q3,HIGH);
+  digitalWrite(t, LOW);
+}
+}
+void backward()
+{
+  v=analogRead(f);
+  if(v<550)
+  {
+  Serial.println("");
+  Serial.println("Backward Close");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,HIGH);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, HIGH);
+  }
+  if(v>551)
+  {
+  Serial.println("");
+  Serial.println("Backward Open");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,HIGH);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, LOW);
+  }
+  
+}
+void left()
+{
+  v=analogRead(f);
+  if(v<550)
+  {
+  Serial.println("");
+  Serial.println("Left Close");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,HIGH);
+  digitalWrite(t, HIGH);
+  }
+  if(v>551)
+  {
+  Serial.println("");
+  Serial.println("Left Open");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,HIGH);
+  digitalWrite(t, LOW);
+  }
+}
+void right()
+{
+  v=analogRead(f);
+  if(v<550)
+  {
+  Serial.println("");
+  Serial.println("Right Close");
+ digitalWrite(Q1,HIGH);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, HIGH);
+  }
+  if(v>551)
+  {
+  Serial.println("");
+  Serial.println("Right Open");
+  digitalWrite(Q1,HIGH);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, LOW);
+  }
+}
+void stop_()
+{
+  v=analogRead(f);
+  if(v<550)
+  {
+  Serial.println("");
+  Serial.println("stop Close");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, HIGH);
+  }
+   if(v>551)
+  {
+  Serial.println("");
+  Serial.println("stop Open");
+  digitalWrite(Q1,LOW);
+  digitalWrite(Q2,LOW);
+  digitalWrite(Q3,LOW);
+  digitalWrite(t, LOW);
+  }
+}
+
